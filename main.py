4a871e4a2256c8324ec3075a46dd3d26fb1d0aa4
24-python <24_python@mail.ru>
2024-05-24 19:18:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра ТИР")
 
-icon = pygame.image.load("icon.png")
+icon = pygame.image.load("img/icon.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
 
 
 running = True
