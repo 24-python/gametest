1d380737910538e5b889ab6281b49d14739c1c8b
24-python <24_python@mail.ru>
2024-05-26 19:17:53 +0300

    уменьшение числа строк

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import pygame
 import random
-
 pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра ТИР")
-
 icon = pygame.image.load("img/icon.jpg")
 pygame.display.set_icon(icon)
-
 target_img = pygame.image.load("img/target.png")
 target_widht = 80
 target_height = 80
-
 target_x = random.randint(0, SCREEN_WIDTH - target_widht)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-
-
-
 running = True
 while running:
      screen.fill(color)
@@ -33,9 +24,6 @@ while running:
              if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
                   target_x = random.randint(0, SCREEN_WIDTH - target_widht)
                   target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
      screen.blit(target_img, (target_x, target_y))
      pygame.display.update()
-
-
 pygame.quit()
